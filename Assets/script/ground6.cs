fecd2dc743815cb6af93574583f44173ad55394d
natumeno <49mssk@gmail.com>
2019-07-21 16:15:00 +0900

    first commit

---
+++ Assets/script/ground6.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ground6 : MonoBehaviour {
+
+	//オブジェクトが衝突したとき
+	void OnCollisionEnter(Collision collision) {
+		Physics.gravity = new Vector3(0, 9.81f, 0);
+	}
+}
---
+++ Assets/script/playercontroller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour {
+
+
+	void FixedUpdate ()
+	{
+		float x =  -Input.GetAxis("Horizontal");
+		float z = -Input.GetAxis("Vertical");
+
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+		// xとzに10をかけて押す力をアップ
+		rigidbody.AddForce(x * 9, 0, z * 9);
+	}
+}
